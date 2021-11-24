c5fba89acbbf909db8af53a0a5f573ebc068f6ab
thnguyen996 <th.nguyen@g.skku.edu>
2021-11-24 14:00:33 +0900

    first commit

---
+++ .config/fish/config.fish
@@ -0,0 +1,25 @@
+if status --is-interactive
+    set -U fish_user_paths "$HOME/anaconda3/bin" $fish_user_paths
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+    eval /home/th.nguyen/anaconda3/bin/conda "shell.fish" "hook" $argv | source
+# <<< conda initialize <<<
+
+# Aliases:
+    alias vi="nvim"
+    alias openf="xdg-open"
+    alias ide="./tmux_ide.sh"
+    alias config="git --git-dir=$HOME/dotfiles --work-tree=$HOME"
+    fish_vi_key_bindings
+
+    set PATH "$HOME/.npm-global/bin:$PATH"
+    set -x SHELL /bin/bash
+    conda activate distiller
+
+    set --local AUTOJUMP_PATH /home/th.nguyen/.linuxbrew/share/autojump/autojump.fish
+    if test -e $AUTOJUMP_PATH
+        source $AUTOJUMP_PATH
+    end
+# Commands to run in interactive sessions can go here
+end
+
